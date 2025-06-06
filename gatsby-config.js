f5f8428548dc615d3ab4ffc5026c5d0693386eee
cayubweeums <calebwill165@gmail.com>
2025-06-05 21:17:30 -0500

    feat: remove plugins, add deploy script, adds temp siteUrl value

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,33 +3,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `\\ reflux \\`,
-    siteUrl: `https://www.yourdomain.com`
+    title: `reflux.cloud`,
+    siteUrl: `https://never-run.com`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: `Reflux - Security Solutions`,
-      short_name: `Reflux`,
-      start_url: `/`,
-      background_color: `#7C3AED`,
-      theme_color: `#7C3AED`,
-      display: `minimal-ui`,
-      icon: `static/favicon-32x32.png`,
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+  plugins: [],
+}
